ac0c13366aa831d409f90c7e31bf312a41e0e6b6
YuHsaunTang <tangbao0828@gmail.com>
2019-05-13 21:15:23 +0800

    create profile

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,3 +1,4 @@
+  
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  show=false;
+
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
