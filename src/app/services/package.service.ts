a19956b62fc6f1f44cf13cfb9ba28b928fa72ba1
YuHsaunTang <tangbao0828@gmail.com>
2019-05-29 12:07:40 +0800

    抓package資料

--- src/app/services/package.service.ts
+++ src/app/services/package.service.ts
@@ -1,21 +1,57 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
 
-export interface Package {
-  uid:string;
-  title:string;
-  address?:string;
-  date?:string;
-  email?:string;
-  gender?:string;
-  phone?:string;
-  touron:boolean;
-  imgsrc?:string;
-}
+//--------------
+
+import { Observable, of, ReplaySubject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { AlertController , ToastController } from '@ionic/angular';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { Reference } from '@angular/fire/storage/interfaces';
+import { Url } from 'url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PackageService {
+  packagesCollection: AngularFirestoreCollection<Package>;
+  user: Observable<User>;
+  packages:Observable<Package[]>;
 
-  constructor() { }
+  constructor(
+    private db: AngularFirestore,
+    private afAuth: AngularFireAuth,
+    private storage: AngularFireStorage,
+    private router: Router){
+      this.packages = this.db.collection('packages').valueChanges();
+  }
+
+  getPackages(){
+    return this.packages;
+  }
+}
+
+export interface Package {
+  title?:string;
+  detailsArray?:Array<true>,
+  userRef?:string,
+}
+
+export class User {
+  uid?:string;
+  email?: string;
+  Name?: string;
+  gender?:string;
+  date?:Date;
+  phone?:string;
+  address?:string;
+  userImg?:string;
+  userImgRef?:string;
+  //-----------
+  touron?:boolean;
+  imgsrc?:string;
+  fileRef?:string;
+  userid?:string;
 }
