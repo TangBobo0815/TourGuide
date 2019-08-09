43814b4f38a3527afc2edd407b951441f1b24e7f
apple7032 <apple70321228@gmail.com>
2019-08-10 01:14:12 +0800

    TAB

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
-import { Router } from '@angular/router';
-import { Package } from '../../models/package';
+
 
 @Component({
   selector: 'app-tab1',
