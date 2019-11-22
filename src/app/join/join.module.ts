1a00b49b4db11b1a564d48255fb69730149869e4
jeff <po8621l124@gmail.com>
2019-11-23 00:20:06 +0800

    增加同地區推薦功能

--- src/app/join/join.module.ts
+++ src/app/join/join.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
-
 import { JoinPage } from './join.page';
+import { StarRatingModule } from 'ionic4-star-rating';
+
 
 const routes: Routes = [
   {
@@ -19,6 +19,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    StarRatingModule,
     RouterModule.forChild(routes)
   ],
   declarations: [JoinPage]
